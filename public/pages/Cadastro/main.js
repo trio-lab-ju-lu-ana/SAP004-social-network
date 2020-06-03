7653bf1cb6edda5db11268bc79e146eff5ee1760
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-06-03 16:56:13 -0300

    pagina cadastro ajustada

--- public/pages/Cadastro/main.js
+++ public/pages/Cadastro/main.js
@@ -1,22 +1,27 @@
-export default () => {
-  const container = document.createElement('div');
-  const template = `
-    <br>
-    <form>
-      <label id='register'>Cadastro<label><br><br>
-      <label id='first-name-label'>Insira seu primeiro nome<label><br>
-      <input id='first-name' type='text'><br><br>
-      <label id='last-name-label'>Insira o seu sobrenome<label><br>
-      <input id='last-name' type='text'><br><br>
-      <label id='user-name-label'>Insira um nick<label><br>
-      <input id='user-name' type='text'><br><br>
-      <label id='e-mail-label'>Insira um e-mail<label><br>
-      <input id='e-mail' type='text'><br><br>
-      <label id='password-label'>Insira uma senha<label><br>
-      <input id='password' type='text'><br><br>
-      <button id='register-button' type="submit">Cadastrar!</button><br>
-    </form>
-      `;
-  container.innerHTML += template;
-  return container;
+export const signUp = () => {
+  const signUpContainer = document.createElement('div');
+  
+  signUpContainer.innerHTML = `
+  <form id="register-form">
+  <h2 id='register'>Cadastro</h2>
+  <fieldset>
+    <input id='first-name' type='text' placeholder="nome">
+  </fieldset>
+  <fieldset>
+    <input id='last-name' type='text' placeholder="sobrenome">
+  </fieldset>
+  <fieldset>
+    <input id='e-mail' type='text' placeholder="email">
+  </fieldset>
+  <fieldset>
+    <input id='password' type='text' placeholder="senha">
+  </fieldset>
+  <button id='register-button' type="submit">Cadastrar!</button>
+  <div>já tem uma conta?<a href="#login">login</a></div>
+</form>
+      `//container.innerHTML += template;
+  
+  
+  
+      return signUpContainer;
 };
