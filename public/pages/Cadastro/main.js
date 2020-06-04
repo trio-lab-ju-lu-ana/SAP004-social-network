b4342640cc6fd1f122847205eb5460a7463d6a70
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-06-04 14:17:16 -0300

    error handler pronto

--- public/pages/Cadastro/main.js
+++ public/pages/Cadastro/main.js
@@ -1,4 +1,5 @@
 import {addNewUser} from "./data.js"
+import {handleFirebaseError} from "../../utils/errorHandler.js"
 
 export const signUp = () => {
   const container = document.createElement('div');
@@ -19,23 +20,29 @@ export const signUp = () => {
     <input id='password' type='password' placeholder="senha" required>
   </fieldset>
   <button class="button-loguin id='register-button' type="submit">Cadastrar!</button>
-  <div>já tem uma conta?<a href="#login">login</a></div>
-</form></div>
+  <div class="alert hidden">usuario já registrado</div>
+</form>
+<div>já tem uma conta?<a href="#login">login</a></div>
+</div>
       `
   container.innerHTML += template;
 
   const signUpForm = container.querySelector("#register-form")
+  const errorAlert = container.querySelector(".alert")
 
   const handleSubmmit = (e) => {
     e.preventDefault()
     const email = signUpForm["e-mail"].value
     const password = signUpForm["password"].value
-    addNewUser(email,password).then(()=>{
+    addNewUser(email, password).then(() => {
       window.location.hash = "#feed"
+    }).catch((error) => {
+
+      handleFirebaseError(error, errorAlert)
     })
   }
 
-  container.addEventListener("submit",handleSubmmit)
+  container.addEventListener("submit", handleSubmmit)
 
 
   return container;
