d231ba90c462431acf33d1a8f2fedc7d6e8da816
Ana carolina Ramos <ana.carolina.dos.reis.ramos@gmail.com>
2020-06-15 04:12:34 -0300

    Responsividade Login e Cadastro e Inclusão de Logos

--- public/pages/Cadastro/main.js
+++ public/pages/Cadastro/main.js
@@ -1,57 +1,54 @@
-import {
-  addNewUser
-} from "./data.js"
-import {
-  handleFirebaseError
-} from "../../utils/errorHandler.js"
+import { addNewUser } from './data.js';
+import { handleFirebaseError } from '../../utils/errorHandler.js';
 
 export const signUp = () => {
   const container = document.createElement('div');
 
   const template = `
-  <div class="align">
-    <form class="form" id="register-form">
-      <h2 id='register'>Cadastro</h2>
-      <fieldset class="textarea">
-        <input id='first-name' type='text' placeholder="Nome">
-      </fieldset>
-      <fieldset class="textarea">
-        <input id='last-name' type='text' placeholder="Sobrenome">
-      </fieldset>
-      <fieldset class="textarea">
-        <input id='e-mail' type='email' placeholder="E-mail" required>
-      </fieldset>
-      <fieldset class="textarea">
-        <input id='password' type='password' placeholder="Senha" required>
-      </fieldset>
-      <button class="button-login id='register-button' type="submit">Cadastrar!</button>
-      <div class="alert hidden">Usuário já registrado!</div>
-      <div>Já tem uma conta? <a href="#login">Login</a></div>
-    </form>
-</div>
-      `
+  <div class='align-items-login'>
+    <div class='align-logo'>
+       <img class='logo-login' src='../images/AthenaLogoQuadrado.png'>     
+    </div>
+    <div class="align">
+      <form class="form" id="register-form">
+        <h2 class='text-description-register' id='register'>Cadastre-se - É rápido!</h2>
+        <fieldset class="textarea">
+          <input id='first-name' type='text' placeholder="Nome">
+        </fieldset>
+        <fieldset class="textarea">
+          <input id='last-name' type='text' placeholder="Sobrenome">
+        </fieldset>
+        <fieldset class="textarea">
+          <input id='userEmail' type='email' placeholder="E-mail" required>
+        </fieldset>
+        <fieldset class="textarea">
+          <input id='userPassword' type='password' placeholder="Senha" required>
+        </fieldset>
+        <button class="button-login" id='register-button' type="submit">Cadastrar!</button><br>
+        <div class="alert hidden">Usuário já registrado!</div>
+        <div>Já tem uma conta?<a href="#login">Login</a></div>
+      </form>
+    </div>
+  </div>
+      `;
   container.innerHTML += template;
 
-  const signUpForm = container.querySelector("#register-form")
-  const errorAlert = container.querySelector(".alert")
+  const signUpForm = container.querySelector('#register-form');
+  const errorAlert = container.querySelector('.alert');
 
   const handleSubmmit = (e) => {
-    e.preventDefault()
-    const email = signUpForm["e-mail"].value
-    const password = signUpForm["password"].value
+    e.preventDefault();
+    const email = signUpForm.userEmail.value;
+    const password = signUpForm.userPassword.value;
 
     addNewUser(email, password).then(() => {
-      window.location.hash = "#feed"
+      window.location.hash = '#feed';
     }).catch((error) => {
+      handleFirebaseError(error, errorAlert);
+    });
+  };
 
-      handleFirebaseError(error, errorAlert)
-    })
-  }
-
-
-
-  container.addEventListener("submit", handleSubmmit)
-
+  container.addEventListener('submit', handleSubmmit);
 
   return container;
 };
--- public/pages/Login/main.js
+++ public/pages/Login/main.js
@@ -1,56 +1,61 @@
-import {
-  loginUser
-} from "./data.js"
-import {
-  handleFirebaseError
-} from "../../utils/errorHandler.js"
+import { loginUser } from './data.js';
+import { handleFirebaseError } from '../../utils/errorHandler.js';
 
 export const login = () => {
   const container = document.createElement('div');
 
   const template = `
-  <div class="align">
-    <form class="form" id="login-form">
-      <h2>Login</h2>
-      <fieldset class="textarea">
-        <input  id='email' type='text' type="email" placeholder="E-mail">
-      </fieldset>
-      <fieldset class="textarea">
-        <input id='password' type='password' placeholder="Password">
-      </fieldset>
-      <button class="button-login" id='entrar' type="submit">Login</button>
-      <div class="alert hidden">Login ou senha incorretos!</div>
-      <div>Não tem uma Conta?
-        <a href="#register">Cadastre-se!</a>
-      </div>
-      <div>
-        <p> Ou entre com:</p>
-      </div>
-      <div class='container-buttons-login'>
-        <button class="button"><i class="fab fa-2x  fa-google"></i></button><br>
-        <button class="button"><i class="fab fa-2x fa-facebook"></i></button>
-      </div>
-    </form>
+  <div class='align-items-login'>
+    <div class='align-logo'>
+       <img class='logo-login' src='../images/AthenaLogoQuadrado.png'>     
+    </div>
+    <div class='align'>
+      <form class="form" id='login-form'>
+        <div class='container-description'>
+          <h2 class='text-description'>Bem vinda a Athena Hideout</h2>
+          <h2 class='text-description'>A rede social para mulheres gamers!</h2>
+        </div>
+        <fieldset class='textarea'>
+          <input  id='userEmail' type='text' type='email' placeholder='E-mail'>
+        </fieldset>
+        <fieldset class='textarea'>
+          <input id='userPassword' type='password' placeholder='Password'>
+        </fieldset>
+        <button class='button-login' id='entrar' type='submit'>Login</button>
+        <div class='alert hidden'>Login ou senha incorretos!</div>
+        <div>
+          <p> Ou entre com:</p>
+        </div>
+        <div class='container-buttons-login'>
+          <button><i class='fab fa-2x  fa-google'></i></button>
+        </div>
+        <div>
+          <p>Não tem uma Conta?
+            <a href='#register'>Cadastre-se!</a>
+          </p>
+        </div>
+      </form>
+  </div>
 </div>
       `;
   container.innerHTML += template;
 
-  const loginForm = container.querySelector("#login-form")
-  const errorAlert = container.querySelector(".alert")
+  const loginForm = container.querySelector('#login-form');
+  const errorAlert = container.querySelector('.alert');
 
 
   const handleLogin = (e) => {
-    e.preventDefault()
-    const email = loginForm["email"].value
-    const password = loginForm["password"].value
+    e.preventDefault();
+    const email = loginForm.userEmail.value;
+    const password = loginForm.userPassword.value;
     loginUser(email, password).then(() => {
-      window.location.hash = "#feed"
+      window.location.hash = '#feed';
     }).catch((error) => {
-      handleFirebaseError(error, errorAlert)
-    })
-  }
+      handleFirebaseError(error, errorAlert);
+    });
+  };
 
-  loginForm.addEventListener("submit", handleLogin)
+  loginForm.addEventListener('submit', handleLogin);
 
   return container;
 };
