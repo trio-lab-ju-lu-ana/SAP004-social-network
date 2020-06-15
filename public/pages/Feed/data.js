631c169133876d1aba163be1b9f23fb0384338f5
Luana Andrade <luana.las21@gmail.com>
2020-06-15 12:12:26 -0300

    post arrumado, alteracao no main e data feed

--- public/pages/Feed/data.js
+++ public/pages/Feed/data.js
@@ -23,6 +23,26 @@ export const renderAllPosts = (feedContainer) => {
        querySnapshot.forEach((doc)=> {
            posts.push(doc.data());
        })
-       feedContainer.innerHTML = posts.map(post => `<p>${post.text}</p>`).join("")
+       feedContainer.innerHTML = posts.map(post => `
+       <div>
+      <div class='container-created-post'>
+        <div class='container-info-post'>
+          <p>An user name and the post status go here</p>
+          <button class="button" title='Like'>
+            <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+          </button>
+        </div>
+        <div id="all-posts" class='posted-message'>
+        <p>${post.text}</p>
+        </div>
+        <div class='container-buttons'>
+            <button class="button" title='Like'>
+              <i class="far fa-star"></i>
+            </button>
+            <button class="button" title='Share'>
+              <i class="fas fa-share-square"></i>
+            </button>
+          </div>
+      </div>`).join("")
     })
 }
