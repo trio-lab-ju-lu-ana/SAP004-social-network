8e3a3551428a3e6634e112c4880a5e0522c02021
Ana carolina Ramos <ana.carolina.dos.reis.ramos@gmail.com>
2020-06-15 04:14:39 -0300

    Responsividade Login e Cadastro e Inclusão de Logos

--- public/index.js
+++ public/index.js
@@ -1,49 +1,44 @@
-import {login}  from './pages/Login/main.js';
-import {feed} from './pages/Feed/main.js'
-import {signUp} from './pages/Cadastro/main.js';
-//import Home from './pages/Home/main.js';
+import { login } from './pages/Login/main.js';
+import { feed } from './pages/Feed/main.js';
+import { signUp } from './pages/Cadastro/main.js';
+// import Home from './pages/Home/main.js';
 
 const root = document.querySelector('#root');
 
-const renderRoute = (root,route,title) => {
-    document.title = title
-    root.innerHTML = ""
-    root.appendChild(route)
-}
+const renderRoute = (route, title) => {
+  document.title = title;
+  root.innerHTML = '';
+  root.appendChild(route);
+};
 
-
-    const routes = [{
-        route: "login",
-        html: login(),
-        title: "Login"
-    },
-    {
-        route: "register",
-        html: signUp(),
-        title: "Cadastro"
-    },
-    {
-        route: "feed",
-        html: feed(),
-        title: "Feed",
-        private: true
-    },
-   
-]
+const routes = [{
+  route: 'login',
+  html: login(),
+  title: 'Login',
+},
+{
+  route: 'register',
+  html: signUp(),
+  title: 'Cadastro',
+},
+{
+  route: 'feed',
+  html: feed(),
+  title: 'Feed',
+  private: true,
+},
+];
 
 const handleRoutes = () => {
-    const route = routes.find(({route}) => window.location.hash === `#${route}`)
-    if (!route) {
-        window.location.hash = "#login"
-    } else if (route.private && !localStorage.getItem("user")){
-        window.location.hash = "#login"
-    }else {
-        renderRoute(root, route.html,route.title)
-    }
+  const route = routes.find(({ route }) => window.location.hash === `#${route}`);
+  if (!route) {
+    window.location.hash = '#register';
+  } else if (route.private && !localStorage.getItem('user')) {
+    window.location.hash = '#register';
+  } else {
+    renderRoute(route.html, route.title);
+  }
+};
 
-
-}
-
-
-window.addEventListener("load", handleRoutes)
-window.addEventListener("hashchange", handleRoutes)
+window.addEventListener('load', handleRoutes);
+window.addEventListener('hashchange', handleRoutes);
